89a2663e62944bee22e7719b3a66b5923ab75a10
storm-ptr <storm-ptr@users.noreply.github.com>
2023-04-14 11:50:41 +0000

    deploy: 650ac69610a2119d29827b121f7b9352f231b17b

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['data_0',['data',['../classstep20_1_1suffix__array.html#ab76603fdc89be18e53a8dd9ad38d79a9',1,'step20::suffix_array::data()'],['../classstep20_1_1suffix__tree.html#a98f7401c93b1bc943fcc07d90310ff53',1,'step20::suffix_tree::data() const']]],
-  ['depth_5ffirst_5fsearch_1',['depth_first_search',['../classstep20_1_1suffix__tree.html#a13a071214ae0bda9b555c5ba213374a3',1,'step20::suffix_tree']]],
+  ['depth_5ffirst_5fsearch_1',['depth_first_search',['../classstep20_1_1suffix__tree.html#ac25f593f4b189dd8b3d354061d47e52e',1,'step20::suffix_tree']]],
   ['difference_5ftype_2',['difference_type',['../structstep20_1_1generator_1_1iterator.html#a8cdf4fc83c99f341f91bd9ce5b7e58e9',1,'step20::generator::iterator']]]
 ];
