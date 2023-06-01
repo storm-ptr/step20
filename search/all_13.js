9ea9a71847ae793df70b809db5a5061cb496a203
storm-ptr <storm-ptr@users.noreply.github.com>
2023-06-01 07:47:44 +0000

    deploy: af0790ab94e8f62fbe7fcaf3fd981e4183afad44

--- search/all_13.js
+++ search/all_13.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['_7esuffix_5farray_0',['~suffix_array',['../classstep20_1_1suffix__array.html#adcb1f1e9e26953a1b6ac006b007e03ee',1,'step20::suffix_array']]],
-  ['_7esuffix_5ftree_1',['~suffix_tree',['../classstep20_1_1suffix__tree.html#a192a76bd4753579b13d0ff4970bcc09f',1,'step20::suffix_tree']]]
+  ['_7ecache_0',['~cache',['../classstep20_1_1least__frequently__used_1_1cache.html#ac0e33d5bc1b510fff91deb5566e8f759',1,'step20::least_frequently_used::cache::~cache()'],['../classstep20_1_1least__recently__used_1_1cache.html#af86c32b3ded4b0dc96b8b7765369c9ed',1,'step20::least_recently_used::cache::~cache()']]],
+  ['_7esuffix_5farray_1',['~suffix_array',['../classstep20_1_1suffix__array.html#adcb1f1e9e26953a1b6ac006b007e03ee',1,'step20::suffix_array']]],
+  ['_7esuffix_5ftree_2',['~suffix_tree',['../classstep20_1_1suffix__tree.html#a192a76bd4753579b13d0ff4970bcc09f',1,'step20::suffix_tree']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['operator_2a_0',['operator*',['../structstep20_1_1generator_1_1iterator.html#abc45aeea37c9e41397611dd6962cc144',1,'step20::generator::iterator']]],
   ['operator_2b_2b_1',['operator++',['../structstep20_1_1generator_1_1iterator.html#aef3105d67c175b7b13dc3500b223a1ba',1,'step20::generator::iterator::operator++(int)'],['../structstep20_1_1generator_1_1iterator.html#a98ae3e27bf553379f3dc8f2f6d74ee21',1,'step20::generator::iterator::operator++()']]],
-  ['operator_3d_3d_2',['operator==',['../structstep20_1_1generator_1_1iterator.html#ad5ec20eda7e6c94af47b1b1f5d35a5bd',1,'step20::generator::iterator']]],
-  ['operator_5b_5d_3',['operator[]',['../classstep20_1_1suffix__array.html#a1910d82356043e669796891492f6566e',1,'step20::suffix_array']]]
+  ['operator_3d_2',['operator=',['../classstep20_1_1least__frequently__used_1_1cache.html#a477390282f0c1cdf4a9b047c17955e00',1,'step20::least_frequently_used::cache::operator=(cache &amp;&amp;)=default'],['../classstep20_1_1least__frequently__used_1_1cache.html#a157417fb10f57987e82f6f732ac50216',1,'step20::least_frequently_used::cache::operator=(const cache &amp;)=delete'],['../classstep20_1_1least__recently__used_1_1cache.html#ad6ba19faec1d41b706d1024a19a8fd96',1,'step20::least_recently_used::cache::operator=(cache &amp;&amp;)=default'],['../classstep20_1_1least__recently__used_1_1cache.html#af05da5f38b5666459d36c36e3a44602e',1,'step20::least_recently_used::cache::operator=(const cache &amp;)=delete']]],
+  ['operator_3d_3d_3',['operator==',['../structstep20_1_1generator_1_1iterator.html#ad5ec20eda7e6c94af47b1b1f5d35a5bd',1,'step20::generator::iterator']]],
+  ['operator_5b_5d_4',['operator[]',['../classstep20_1_1suffix__array.html#a1910d82356043e669796891492f6566e',1,'step20::suffix_array']]]
 ];
